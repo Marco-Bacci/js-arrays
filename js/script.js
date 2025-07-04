77b7e75bb7a91914a7c75f0840b1fa50c34de71b
Marco Bacci <marcobacci94@gmail.com>
2025-07-04 10:01:48 +0200

    feat: (bonus 1) do cicle for

--- js/script.js
+++ js/script.js
@@ -57,17 +57,25 @@ const teachers = [
 // // e salva il risultato nella variabile reversedTeachers
 // const reversedTeachers = teachers.reverse();
 // console.log(reversedTeachers)
+const tempArray = [];
+for(let i = teachers.length - 1; i>=0; i--){
+  tempArray.push(teachers[i])
+}
+const reversedTeachers = tempArray
+console.log(reversedTeachers)
+
+
 
 // // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = []
-for (let i=0; i<teachers.length; i++ ) {
-const fiveTeachers = teachers[i]
-if (fiveTeachers.length >=5) {
-  longNames.push(fiveTeachers)
-}
-}
-console.log(longNames)
+// const longNames = []
+// for (let i=0; i<teachers.length; i++ ) {
+// const fiveTeachers = teachers[i]
+// if (fiveTeachers.length >=5) {
+//   longNames.push(fiveTeachers)
+// }
+// }
+// console.log(longNames)
 
 
 // // 3. Rimuovi 'Ed' dall'array teachers
